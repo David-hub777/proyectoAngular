42aaa543ad0da7ea2aebaffd6f7b066c817a2fff
David <david25042001@gmail.com>
2024-01-19 11:15:48 +0100

    Version estable de los pipes (ahora si)

--- src/app/Alumno/AlumnoLista.component.ts
+++ src/app/Alumno/AlumnoLista.component.ts
@@ -23,8 +23,6 @@ export class AlumnoComponent {
         { nombre: 'Miguel', apellidos: 'Indurain', direccion: 'Pamplona_City', fnac: '7/16/1949', sexo: 'Masculino' },
         { nombre: 'Jose María', apellidos: 'Jiménez', direccion: 'Cuenca_City', fnac: '5/5/1974', sexo: 'Masculino' },
         { nombre: 'Alberto', apellidos: 'Contador', direccion: 'Albacete_City', fnac: '12/6/1951', sexo: 'Masculino' },
-        { nombre: 'Sailor', apellidos: 'Moon', direccion: 'Tokyo', fnac: '6/30/2007', sexo: 'Femenino' },
-        { nombre: 'Luffy', apellidos: 'Monkey D.', direccion: 'One Piece Island', fnac: '5/5/2002', sexo: 'Masculino' },
         { nombre: 'Ainz', apellidos: 'Ooal Gown', direccion: 'Gran Tumba de Nazarick', fnac: '7/30/0020', sexo: 'Masculino' },
         { nombre: 'Albedo', apellidos: 'White', direccion: 'Gran Tumba de Nazarick', fnac: '3/15/0500', sexo: 'Femenino' },
         { nombre: 'Shalltear', apellidos: 'Bloodfallen', direccion: 'Gran Tumba de Nazarick', fnac: '12/24/1800', sexo: 'Femenino' },
